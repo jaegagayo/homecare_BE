7a48910f8b24b380c42564abc6733fcea3b4602b
GlasSua <49929863+Saelma@users.noreply.github.com>
2025-07-27 01:57:48 +0900

    rename : Swagger 설명 수정

--- homecare/src/main/java/jaega/homecare/domain/users/controller/UserController.java
+++ homecare/src/main/java/jaega/homecare/domain/users/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/api/user")
 public interface UserController {
 
-    @Operation(summary = "수요자(고객) 회원가입 API", description = "입력받은 정보로 수요자의 회원가입을 진행합니다.")
+    @Operation(summary = "유저 기본 회원가입 API", description = "입력받은 정보로 유저의 기본 회원가입을 진행합니다.")
     @ApiResponse(responseCode = "204", description = "유저 생성 성공")
     @PostMapping("/register")
     ResponseEntity<Void> createConsumer(UserCreateRequest request);
