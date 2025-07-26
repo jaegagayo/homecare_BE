01a19304f76c1c19f81c9654593f7013cdbf5fe7
GlasSua <49929863+Saelma@users.noreply.github.com>
2025-07-27 02:29:25 +0900

    feat : user 도메인 userId 조건 조회 서비스 추가

--- homecare/src/main/java/jaega/homecare/domain/users/service/query/UserQueryService.java
+++ homecare/src/main/java/jaega/homecare/domain/users/service/query/UserQueryService.java
@@ -1,9 +1,21 @@
 package jaega.homecare.domain.users.service.query;
 
+import jaega.homecare.domain.users.entity.User;
+import jaega.homecare.domain.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class UserQueryService {
+    private final UserRepository userRepository;
+
+    public User getUser(UUID userId){
+        return userRepository.findByUserId(userId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "유저 정보를 찾을 수 없습니다."));
+    }
 }
