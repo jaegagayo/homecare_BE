696aaa159be2c6ac01de54bb846db42d978d100e
GlasSua <49929863+Saelma@users.noreply.github.com>
2025-07-27 16:28:35 +0900

    remove : user의 column 어노테이션 삭제
    의미없이 쓰이지 않는 어노테이션 삭제

--- homecare/src/main/java/jaega/homecare/domain/users/entity/User.java
+++ homecare/src/main/java/jaega/homecare/domain/users/entity/User.java
@@ -17,16 +17,12 @@ public class User {
 
     private UUID userId;
 
-    @Column
     private String name;
 
-    @Column
     private String email;
 
-    @Column
     private String password;
 
-    @Column
     private String phone;
 
     @Enumerated(EnumType.STRING)
