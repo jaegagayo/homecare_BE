d0126901e4dd05b590a7fffe9aaa21eb5f74becc
Hyerim Jeon <86788892+JeonHyerim86@users.noreply.github.com>
2025-07-31 21:25:24 +0900

    feat: Caregiver Entity 생성

---
+++ homecare/src/main/java/jaega/homecare/domain/caregiver/entity/ServiceType.java
@@ -0,0 +1,9 @@
+package jaega.homecare.domain.caregiver.entity;
+
+public enum ServiceType {
+    VISITING_CARE,      // 방문 요양
+    VISITING_BATH,      // 방문 목욕
+    VISITING_NURSING,   // 방문 간호
+    DAY_NIGHT_CARE,     // 주야간 보호
+    RESPITE_CARE        // 단기 보호
+}
