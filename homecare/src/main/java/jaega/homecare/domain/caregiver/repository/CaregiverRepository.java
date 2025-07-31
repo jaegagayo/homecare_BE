6b44dc8de3dc2bb1d52b9562411fad116d49fe93
GlasSua <49929863+Saelma@users.noreply.github.com>
2025-08-01 00:30:12 +0900

    feat : UUID 기반 특정 요양보호사 조회 API 구현

--- homecare/src/main/java/jaega/homecare/domain/caregiver/repository/CaregiverRepository.java
+++ homecare/src/main/java/jaega/homecare/domain/caregiver/repository/CaregiverRepository.java
@@ -3,5 +3,9 @@ package jaega.homecare.domain.caregiver.repository;
 import jaega.homecare.domain.caregiver.entity.Caregiver;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface CaregiverRepository extends JpaRepository<Caregiver, Long>{
+    Optional<Caregiver> findByCaregiverId(UUID caregiverId);
 }
