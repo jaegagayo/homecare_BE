fc6a8b5e4c4f8cf37540a4843a25a0d6e2eb3b4d
GlasSua <49929863+Saelma@users.noreply.github.com>
2025-08-02 00:25:45 +0900

    feat : 매칭 알고리즘 연동 및 사전 필터링 적용

--- homecare/src/main/java/jaega/homecare/domain/caregiver/repository/CaregiverRepository.java
+++ homecare/src/main/java/jaega/homecare/domain/caregiver/repository/CaregiverRepository.java
@@ -1,11 +1,15 @@
 package jaega.homecare.domain.caregiver.repository;
 
 import jaega.homecare.domain.caregiver.entity.Caregiver;
+import jaega.homecare.domain.caregiver.entity.CaregiverStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CaregiverRepository extends JpaRepository<Caregiver, Long>{
     Optional<Caregiver> findByCaregiverId(UUID caregiverId);
+
+    List<Caregiver> findAllByStatus(CaregiverStatus status);
 }
