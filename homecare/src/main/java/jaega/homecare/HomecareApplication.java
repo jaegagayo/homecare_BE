e9d5165c998aaefd1e4cb1d6990a689509ec4404
GlasSua <49929863+Saelma@users.noreply.github.com>
2025-08-13 14:38:05 +0900

    feat : 데이터 생성, 변경을 위한 추적 클래스 추가

--- homecare/src/main/java/jaega/homecare/HomecareApplication.java
+++ homecare/src/main/java/jaega/homecare/HomecareApplication.java
@@ -2,8 +2,10 @@ package jaega.homecare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HomecareApplication {
 
 	public static void main(String[] args) {
