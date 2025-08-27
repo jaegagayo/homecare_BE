47e254ce5c2ff2c3acc64c2a82ce1555540ba4c4
GlasSua <49929863+Saelma@users.noreply.github.com>
2025-08-28 01:58:10 +0900

    ref : consumer 컨트롤러 구조 통합

--- homecare/src/main/java/jaega/homecare/domain/consumer/controller/ConsumerController.java
+++ homecare/src/main/java/jaega/homecare/domain/consumer/controller/ConsumerController.java
@@ -3,14 +3,27 @@ package jaega.homecare.domain.consumer.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jaega.homecare.domain.blacklist.dto.req.CreateBlacklistByConsumerRequest;
+import jaega.homecare.domain.blacklist.dto.res.GetBlacklistByConsumerResponse;
 import jaega.homecare.domain.consumer.dto.req.ConfirmCaregiverRequest;
 import jaega.homecare.domain.consumer.dto.req.ConsumerProfileUpdateRequest;
 import jaega.homecare.domain.consumer.dto.req.ConsumerSignupRequest;
 import jaega.homecare.domain.consumer.dto.req.ConsumerUpdateRequest;
 import jaega.homecare.domain.consumer.dto.res.*;
+import jaega.homecare.domain.review.dto.req.CreateReviewRequest;
+import jaega.homecare.domain.review.dto.res.ConsumerPendingReviewResponse;
+import jaega.homecare.domain.review.dto.res.ConsumerReviewResponse;
+import jaega.homecare.domain.review.dto.res.GetReviewResponse;
+import jaega.homecare.domain.serviceRequest.dto.req.ConsumerServiceRequest;
+import jaega.homecare.domain.serviceRequest.dto.req.UpdateServiceRequest;
+import jaega.homecare.domain.serviceRequest.dto.res.GetCreateServiceResponse;
+import jaega.homecare.domain.serviceRequest.dto.res.GetServiceRequestById;
+import jaega.homecare.domain.serviceRequest.dto.res.GetServiceRequestResponse;
+import jaega.homecare.domain.serviceRequest.entity.ServiceRequestStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.AccessDeniedException;
 import java.util.List;
 import java.util.UUID;
 
@@ -58,4 +71,66 @@ public interface ConsumerController {
     @PutMapping
     ResponseEntity<Void> updateProfile(@RequestParam UUID consumerId,
                                        @RequestBody ConsumerProfileUpdateRequest request);
+
+    @Operation(summary = "블랙리스트 생성 API", description = "요양보호사를 블랙리스트에 추가합니다.")
+    @ApiResponse(responseCode = "201", description = "블랙리스트 생성 성공")
+    @PostMapping("/caregiver")
+    ResponseEntity<UUID> createBlacklistByConsumer(@RequestBody CreateBlacklistByConsumerRequest request);
+
+    @Operation(summary = "블랙리스트 해제 API", description = "요양보호사를 블랙리스트에서 해제합니다.")
+    @ApiResponse(responseCode = "204", description = "블랙리스트 해제 성공")
+    @DeleteMapping("/caregiver")
+    ResponseEntity<Void> deleteBlacklistByConsumer(@RequestParam UUID caregiverBlacklistId);
+
+    @Operation(summary = "신고자별 블랙리스트 조회 API", description = "특정 신고자가 신고한 블랙리스트를 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "블랙리스트 조회 성공")
+    @GetMapping("/consumer/{consumerId}")
+    ResponseEntity<List<GetBlacklistByConsumerResponse>> getBlacklistByConsumer(@PathVariable UUID consumerId);
+
+    @Operation(summary = "리뷰 생성 API", description = "서비스 매칭에 대한 새로운 리뷰를 생성합니다.")
+    @ApiResponse(responseCode = "201", description = "리뷰 생성 성공")
+    @PostMapping
+    ResponseEntity<UUID> createReview(@RequestBody CreateReviewRequest request) throws AccessDeniedException;
+
+    @Operation(summary = "리뷰 조회 API", description = "서비스 매치 ID를 통해 해당 매치의 리뷰를 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "리뷰 조회 성공")
+    @GetMapping("/serviceMatch/{serviceMatchId}")
+    ResponseEntity<GetReviewResponse> getReviewByServiceMatch(@PathVariable UUID serviceMatchId);
+
+    @Operation(summary = "수요자 리뷰 조회 API", description = "수요자가 작성한 리뷰를 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "수요자가 작성한 리뷰 조회 성공")
+    @GetMapping("/{consumerId}/written")
+    ResponseEntity<List<ConsumerReviewResponse>> getWrittenReviews(@PathVariable UUID consumerId);
+
+    @Operation(summary = "수요자가 작성해야할 리뷰 조회 API", description = "수요자가 아직 리뷰를 작성하지 않은 매칭들을 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "수요자가 아직 리뷰를 작성하지 않은 매칭 일정 조회 성공")
+    @GetMapping("/{consumerId}/pending")
+    ResponseEntity<List<ConsumerPendingReviewResponse>> getPendingReviews(@PathVariable UUID consumerId);
+
+    @Operation(summary = "수요자 서비스 요청 API", description = "입력받은 정보로 수요자가 서비스를 요청합니다.")
+    @ApiResponse(responseCode = "204", description = "수요자가 서비스 요청 성공")
+    @PostMapping
+    ResponseEntity<GetCreateServiceResponse> createServiceRequest(@RequestBody ConsumerServiceRequest request);
+
+    @Operation(summary  = "수요자가 신청한 서비스 내역 조회 API", description = "수요자가 신청한 서비스를 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "수요자가 신청한 서비스 내역 조회 성공")
+    @GetMapping
+    ResponseEntity<List<GetServiceRequestResponse>> getConsumerServiceRequest(@RequestParam UUID consumerId);
+
+    @Operation(summary  = "수요자가 신청한 서비스 내역 조회 API (신청 서비스 상태 조건)", description = "수요자가 신청한 서비스를 신청한 서비스의 상태를 조건으로 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "수요자가 신청한 서비스 내역 신청 상태를 조건으로 조회 성공")
+    @GetMapping("/status")
+    ResponseEntity<List<GetServiceRequestResponse>> getConsumerServiceRequestByStatus(@RequestParam UUID consumerId, ServiceRequestStatus status);
+
+
+    @Operation(summary = "수요자가 신청한 서비스 내역 상세 조회", description = "수요자가 신청한 서비스 내역을 서비스 내역의 아이디로 상세 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "수요자가 신청한 서비스 내역 상세 조회 성공")
+    @GetMapping("/{serviceRequestId}")
+    ResponseEntity<GetServiceRequestById> getServiceRequestById(@PathVariable UUID serviceRequestId);
+
+    @Operation(summary = "수요자가 신청한 서비스 신청 정보 수정 API", description = "입력받은 정보로 서비스 신청 정보를 수정합니다.")
+    @ApiResponse(responseCode = "204", description = "서비스 신청 정보 수정 성공")
+    @PutMapping("/request")
+    ResponseEntity<Void> updateServiceRequest(@RequestParam UUID serviceRequestId,
+                                              @RequestBody UpdateServiceRequest request);
 }
--- homecare/src/main/java/jaega/homecare/domain/consumer/controller/ConsumerControllerImpl.java
+++ homecare/src/main/java/jaega/homecare/domain/consumer/controller/ConsumerControllerImpl.java
@@ -1,21 +1,40 @@
 package jaega.homecare.domain.consumer.controller;
 
+import jaega.homecare.domain.blacklist.dto.req.CreateBlacklistByConsumerRequest;
+import jaega.homecare.domain.blacklist.dto.res.GetBlacklistByConsumerResponse;
+import jaega.homecare.domain.blacklist.service.command.BlacklistCommandService;
+import jaega.homecare.domain.blacklist.service.query.BlacklistQueryService;
 import jaega.homecare.domain.consumer.dto.req.ConsumerProfileUpdateRequest;
-import jaega.homecare.domain.consumer.dto.req.ConsumerUpdateRequest;
 import jaega.homecare.domain.consumer.dto.res.*;
 import jaega.homecare.domain.consumer.entity.Consumer;
 import jaega.homecare.domain.consumer.service.query.ConsumerQueryService;
 import jaega.homecare.domain.consumer.dto.req.ConfirmCaregiverRequest;
 import jaega.homecare.domain.consumer.dto.req.ConsumerSignupRequest;
+import jaega.homecare.domain.review.dto.req.CreateReviewRequest;
+import jaega.homecare.domain.review.dto.res.ConsumerPendingReviewResponse;
+import jaega.homecare.domain.review.dto.res.ConsumerReviewResponse;
+import jaega.homecare.domain.review.dto.res.GetReviewResponse;
+import jaega.homecare.domain.review.service.command.ReviewCommandService;
+import jaega.homecare.domain.review.service.query.ReviewQueryService;
 import jaega.homecare.domain.serviceMatch.dto.req.CreateServiceMatchRequest;
 import jaega.homecare.domain.serviceMatch.service.command.ServiceMatchCommandService;
+import jaega.homecare.domain.serviceMatch.service.query.ServiceMatchQueryService;
+import jaega.homecare.domain.serviceRequest.dto.req.ConsumerServiceRequest;
+import jaega.homecare.domain.serviceRequest.dto.req.UpdateServiceRequest;
+import jaega.homecare.domain.serviceRequest.dto.res.GetCreateServiceResponse;
+import jaega.homecare.domain.serviceRequest.dto.res.GetServiceRequestById;
+import jaega.homecare.domain.serviceRequest.dto.res.GetServiceRequestResponse;
 import jaega.homecare.domain.serviceRequest.entity.ServiceRequest;
+import jaega.homecare.domain.serviceRequest.entity.ServiceRequestStatus;
+import jaega.homecare.domain.serviceRequest.service.command.ServiceRequestCommandService;
 import jaega.homecare.domain.serviceRequest.service.query.ServiceRequestQueryService;
 import jaega.homecare.domain.consumer.service.command.ConsumerCommandService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.AccessDeniedException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
@@ -27,8 +46,14 @@ public class ConsumerControllerImpl implements ConsumerController {
 
     private final ConsumerCommandService consumerCommandService;
     private final ServiceMatchCommandService serviceMatchCommandService;
+    private final ServiceMatchQueryService serviceMatchQueryService;
     private final ServiceRequestQueryService serviceRequestQueryService;
+    private final ServiceRequestCommandService serviceRequestCommandService;
     private final ConsumerQueryService consumerQueryService;
+    private final BlacklistCommandService blacklistCommandService;
+    private final BlacklistQueryService blacklistQueryService;
+    private final ReviewCommandService reviewCommandService;
+    private final ReviewQueryService reviewQueryService;
 
     /**
      *
@@ -120,4 +145,102 @@ public class ConsumerControllerImpl implements ConsumerController {
         return ResponseEntity.noContent().build();
     }
 
+    /**
+     *
+     * 블랙리스트 관련 API
+     */
+
+    // 블랙리스트 생성 API
+    @Override
+    public ResponseEntity<UUID> createBlacklistByConsumer(@RequestBody CreateBlacklistByConsumerRequest request) {
+        UUID blacklistId = blacklistCommandService.createBlacklistByConsumer(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(blacklistId);
+    }
+
+    // 블랙리스트 해제 API
+    @Override
+    public ResponseEntity<Void> deleteBlacklistByConsumer(@PathVariable UUID caregiverBlacklistId){
+        blacklistCommandService.deleteBlacklistByConsumer(caregiverBlacklistId);
+        return ResponseEntity.noContent().build();
+    }
+
+    // 신고자별 블랙리스트 조회 API
+    @Override
+    public ResponseEntity<List<GetBlacklistByConsumerResponse>> getBlacklistByConsumer(@PathVariable UUID consumerId) {
+        List<GetBlacklistByConsumerResponse> responses = blacklistQueryService.getBlacklistByConsumer(consumerId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responses);
+    }
+
+    /**
+     * 리뷰 관련 API
+     */
+
+    // 리뷰 생성 API
+    // TODO : JWT 인증 로직 구현 시 직접 consumerId에서 가져오는 게 아닌 인증값에서 가져오도록 할 것 !
+    // TODO : 추가로, Exception 또한 JWT에서 하기 !!
+    @Override
+    public ResponseEntity<UUID> createReview(@RequestBody CreateReviewRequest request) throws AccessDeniedException {
+        UUID reviewId = reviewCommandService.createReview(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(reviewId);
+    }
+
+    // 리뷰 조회 API
+    @Override
+    public ResponseEntity<GetReviewResponse> getReviewByServiceMatch(@PathVariable UUID serviceMatchId) {
+        GetReviewResponse response = reviewQueryService.getReviewByServiceMatch(serviceMatchId);
+        return ResponseEntity.ok(response);
+    }
+
+    //
+    @Override
+    public ResponseEntity<List<ConsumerReviewResponse>> getWrittenReviews(@PathVariable UUID consumerId) {
+        List<ConsumerReviewResponse> reviews = reviewQueryService.getWrittenReviews(consumerId);
+        return ResponseEntity.ok(reviews);
+    }
+
+    @Override
+    public ResponseEntity<List<ConsumerPendingReviewResponse>> getPendingReviews(@PathVariable UUID consumerId) {
+        List<ConsumerPendingReviewResponse> pending = serviceMatchQueryService.getPendingReviews(consumerId);
+        return ResponseEntity.ok(pending);
+    }
+
+    /**
+     * 서비스 요청 API
+     */
+
+    // 수요자 서비스 요청 API
+    @Override
+    public ResponseEntity<GetCreateServiceResponse> createServiceRequest(@RequestBody ConsumerServiceRequest request){
+        GetCreateServiceResponse response = serviceRequestCommandService.createServiceRequest(request);
+        return ResponseEntity.ok(response);
+    }
+
+    // 수요자가 신청한 서비스 내역 조회 API
+    @Override
+    public ResponseEntity<List<GetServiceRequestResponse>> getConsumerServiceRequest(@RequestParam UUID consumerId){
+        List<GetServiceRequestResponse> response = serviceRequestQueryService.findConsumerRequests(consumerId);
+        return ResponseEntity.ok(response);
+    }
+
+    // 수요자가 신청한 서비스 내역 조회 API (신청 서비스 상태 조건)
+    @Override
+    public ResponseEntity<List<GetServiceRequestResponse>> getConsumerServiceRequestByStatus(@RequestParam UUID consumerId, ServiceRequestStatus status){
+        List<GetServiceRequestResponse> response = serviceRequestQueryService.findConsumerRequestsByStatus(consumerId, status);
+        return ResponseEntity.ok(response);
+    }
+
+    // 수요자가 신청한 서비스 내역 상세 조회
+    @Override
+    public ResponseEntity<GetServiceRequestById> getServiceRequestById(UUID serviceRequestId) {
+        GetServiceRequestById response = serviceRequestQueryService.findServiceRequestById(serviceRequestId);
+        return ResponseEntity.ok(response);
+    }
+    
+    @Override
+    public ResponseEntity<Void> updateServiceRequest(@RequestParam UUID serviceRequestId,
+                                                     @RequestBody UpdateServiceRequest request){
+        ServiceRequest serviceRequest = serviceRequestQueryService.getServiceRequest(serviceRequestId);
+        serviceRequestCommandService.updateServiceRequest(request, serviceRequest);
+        return ResponseEntity.noContent().build();
+    }
 }
