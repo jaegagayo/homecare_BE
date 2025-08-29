425632a8a6b065a55c154e90f59327162e3d47a3
GlasSua <49929863+Saelma@users.noreply.github.com>
2025-08-30 00:39:47 +0900

    feat : 누락된 마이페이지 바우처 내역 조회 쿼리 추가

--- homecare/src/main/java/jaega/homecare/domain/voucherUsage/repository/VoucherUsageQueryRepository.java
+++ homecare/src/main/java/jaega/homecare/domain/voucherUsage/repository/VoucherUsageQueryRepository.java
@@ -7,11 +7,17 @@ import jaega.homecare.domain.serviceMatch.entity.QServiceMatch;
 import jaega.homecare.domain.serviceRequest.entity.QServiceRequest;
 import jaega.homecare.domain.serviceRequest.entity.ServiceRequestStatus;
 import jaega.homecare.domain.voucher.entity.QVoucher;
+import jaega.homecare.domain.voucher.entity.Voucher;
 import jaega.homecare.domain.voucherUsage.entity.QVoucherUsage;
+import jaega.homecare.domain.voucherUsage.entity.VoucherUsage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
+
+import static jaega.homecare.domain.voucher.entity.QVoucher.voucher;
+import static jaega.homecare.domain.voucherUsage.entity.QVoucherUsage.voucherUsage;
 
 @Repository
 @RequiredArgsConstructor
@@ -32,5 +38,16 @@ public class VoucherUsageQueryRepository {
                 .fetchOne();
     }
 
+    public List<VoucherUsage> findByVoucherAndStatus(Voucher voucher, MatchStatus status) {
+        return queryFactory
+                .selectFrom(voucherUsage)
+                .join(voucherUsage.serviceMatch)
+                .fetchJoin()
+                .where(
+                        voucherUsage.voucher.eq(voucher),
+                        voucherUsage.serviceMatch.matchStatus.eq(status)
+                )
+                .fetch();
+    }
 
 }
